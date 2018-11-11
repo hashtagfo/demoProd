663f68f45f5b05559f224d8aa3f9aa3215367f46
Shubhamhack7 <shubhamtiwaricr07@gmail.com>
2018-11-11 14:24:08 +0530

    initial

--- src/lu/lllc/DBInfo.java
+++ src/lu/lllc/DBInfo.java
@@ -3,13 +3,13 @@ package lu.lllc;
 public class DBInfo {
 	static String host = "studence-app-mysql"; // The standard name in Tomcat+ MySQL on openshift
 
-	static String DBname = "studence-databse";
+	static String DBname = "studencedatabse";
 	static int port = 3306; // This is for MySQL
 	static String mySQLdbURL = "jdbc:mysql://" + host + ":" + port + "/"
 			+ DBname;
 
-	static String user = "studence123";
-	static String password = "redbinout4you";
+	static String user = "studence";
+	static String password = "redbinoutyou";
 	
 	static String driver = "com.mysql.jdbc.Driver";
 
